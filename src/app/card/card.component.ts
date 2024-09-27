7467ff3a28ecd224a8630df43a49dd8044844d6f
ameetmadan-cleoag <ameet.madan@cleoag.ch>
2024-09-27 12:15:25 +0200

    updates

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -11,4 +11,30 @@ import { Country } from '../types';
 
 export class CardComponent {
   @Input() country!: Country;
+
+  currencyKey!: string;
+  currencySymbol!: string;
+  currencyName!: string;
+  languages!: string;
+  population!: string;
+
+  ngOnChanges() {
+    if (!this.country) {
+      return;
+    }
+
+    if (this.country.population) {
+      this.population = new Intl.NumberFormat('de-CH').format(Number(this.country.population));
+    }
+
+    if (this.country && this.country.currencies) {
+      this.currencyKey = Object.keys(this.country.currencies)[0];
+      this.currencyName = this.country.currencies[this.currencyKey].name;
+      this.currencySymbol = this.country.currencies[this.currencyKey].symbol;
+    }    
+    
+    if (this.country && this.country.languages) {
+      this.languages = Object.values(this.country.languages).join(', ');
+    }
+  }
 }
--- src/app/types.ts
+++ src/app/types.ts
@@ -3,7 +3,7 @@ export type Country = {
       common: string
       official: string
       nativeName: {
-        eng: {
+        [key: string]: {
           official: string
           common: string
         }
@@ -17,7 +17,7 @@ export type Country = {
     status: string
     unMember: boolean
     currencies: {
-      SHP: {
+      [key: string]: {
         name: string
         symbol: string
       }
@@ -30,115 +30,13 @@ export type Country = {
     altSpellings: Array<string>
     region: string
     languages: {
-      eng: string
-    }
-    translations: {
-      ara: {
-        official: string
-        common: string
-      }
-      bre: {
-        official: string
-        common: string
-      }
-      ces: {
-        official: string
-        common: string
-      }
-      cym: {
-        official: string
-        common: string
-      }
-      deu: {
-        official: string
-        common: string
-      }
-      est: {
-        official: string
-        common: string
-      }
-      fin: {
-        official: string
-        common: string
-      }
-      fra: {
-        official: string
-        common: string
-      }
-      hrv: {
-        official: string
-        common: string
-      }
-      hun: {
-        official: string
-        common: string
-      }
-      ita: {
-        official: string
-        common: string
-      }
-      jpn: {
-        official: string
-        common: string
-      }
-      kor: {
-        official: string
-        common: string
-      }
-      nld: {
-        official: string
-        common: string
-      }
-      per: {
-        official: string
-        common: string
-      }
-      pol: {
-        official: string
-        common: string
-      }
-      por: {
-        official: string
-        common: string
-      }
-      rus: {
-        official: string
-        common: string
-      }
-      slk: {
-        official: string
-        common: string
-      }
-      spa: {
-        official: string
-        common: string
-      }
-      srp: {
-        official: string
-        common: string
-      }
-      swe: {
-        official: string
-        common: string
-      }
-      tur: {
-        official: string
-        common: string
-      }
-      urd: {
-        official: string
-        common: string
-      }
-      zho: {
-        official: string
-        common: string
-      }
+      [key: string]: string
     }
     latlng: Array<number>
     landlocked: boolean
     area: number
     demonyms: {
-      eng: {
+      [key: string]: {
         f: string
         m: string
       }
